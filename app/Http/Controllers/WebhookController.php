c285199a49c375fc21d3d68116f8fb20a521fc31
borislemke <dimaslemke@gmail.com>
2016-04-23 12:45:10 +0800

    WebHook test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,6 +12,7 @@ class WebhookController extends Controller
     {
         File::put(storage_path('webhook.json'), json_encode(Input::all()));
 
+        // Added shell command //
         shell_exec('cd /var/www/html && git pull');
 
         return 200;
