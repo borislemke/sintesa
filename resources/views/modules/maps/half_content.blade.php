6245f2cf73f601649d05da3870639607c3edecbe
lewiswebber <hello@fleava.com>
2016-04-26 16:08:30 +0800

    initial

---
+++ resources/views/modules/maps/half_content.blade.php
@@ -0,0 +1,45 @@
+<!-- module: mixed.featurette -->
+<script>
+    var map;
+    var infowindow;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('jimbaran-map'), {
+            center: {
+                lat: {{ $module->data->lat }},
+                lng: {{ $module->data->long }}
+            },
+            scrollwheel: false,
+            zoom: {{ $module->data->zoom }}
+        });
+
+        infowindow = new google.maps.InfoWindow();
+
+        var locations = [];
+        locations = {!! json_encode($module->data->markers) !!};
+
+        var marker, i;
+
+        for (var item in locations) {
+            if (locations.hasOwnProperty(item)) {
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(locations[item]['lat'], locations[item]['long']),
+                    map: map
+                });
+
+                google.maps.event.addListener(marker, 'click', (function (marker, items) {
+                    return function () {
+                        infowindow.setContent('<p style="margin: 0; text-align: center">' + locations[items]['text'] + '<br>' + locations[items]['dist'] + '<br>' + locations[items]['time'] + '</p>');
+                        infowindow.open(map, marker);
+                    }
+                })(marker, item));
+            }
+        }
+    }
+</script>
+<section class="section-block bkg-white">
+    <div id="jimbaran-map" style="height: 540px; width:50%;  float:left"></div>
+    <div style="width:50%; float:left">{{ $module->data->content }}</div>
+    <div class="clear"></div>
+</section>
+<!-- module_end: mixed.featurette -->
