fe8fb39b32d2c1e7ab1fd53cc4c572ce91034071
lewiswebber <hello@fleava.com>
2016-05-03 20:00:38 +0800

    wellness white

--- resources/views/modules/custom/wellness_header.blade.php
+++ resources/views/modules/custom/wellness_header.blade.php
@@ -4,20 +4,22 @@
         <ul class="tms-slides">
 
             <li class="tms-slide" data-image data-force-fit data-overlay-bkg-color="#131027"
-                data-overlay-bkg-opacity="0.1">
+                data-overlay-bkg-opacity="0.5">
                 <div class="tms-content">
                     <div class="tms-content-inner left left-on-mobile v-align-bottom">
                         <div class="row">
-                            <div class="width-6 offset-3 wellness-header-logo-wrapper">
-                                <svg class="wellness-header-logo wellness-header-logo"><use xlink:href="{{ Request::path() }}#fleava-logo-wellness"></use></svg>
-                                <h2>The Sintesa Wellness<br><small>Jimbaran</small></h2>
+                            <div class="width-6 offset-3 wellness-header-logo-wrapper color-white">
+                                <svg class="wellness-header-logo wellness-header-logo tms-caption" data-animate-in="opacity:0;transY:30px;duration:800ms;easing:easeFastSlow;"
+                                       data-no-scale><use xlink:href="{{ Request::path() }}#fleava-logo-wellness"></use></svg>
+                                <h2 class="tms-caption mt-30" data-animate-in="opacity:0;transY:30px;duration:800ms;delay:200ms;easing:easeFastSlow;"
+                                       data-no-scale>The Sintesa Wellness<br><small>Jimbaran</small></h2>
                             </div>
                         </div>
                     </div>
                 </div>
-                <img data-src="media/slider/wellness.jpg" src="media/blank.png" alt="Sintesa Wellness Center"/>
+                <img data-src="media/slider/slide-3.jpg" src="media/blank.png" alt="Sintesa Wellness Center"/>
                 <div class="blur-effect">
-                  <div style="background-image:url(media/slider/wellness.jpg)"></div>
+                  <div style="background-image:url(media/slider/slide-3.jpg)"></div>
                 </div>
             </li>
 
