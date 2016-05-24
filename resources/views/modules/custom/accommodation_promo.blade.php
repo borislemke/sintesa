b082a91a7b34c95b8824c9e9f84f27b28f55b57d
borislemke <dimaslemke@gmail.com>
2016-05-24 21:33:03 +0800

    promo message

--- resources/views/modules/custom/accommodation_promo.blade.php
+++ resources/views/modules/custom/accommodation_promo.blade.php
@@ -3,7 +3,9 @@
     <div class="row">
         <div class="column width-3 offset-1">
             <h2 class="mb-20">Promotion</h2>
+            @if(FALSE)
             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            @endif
         </div>
         <div class="column width-7">
             <div id="grid-1" class="row content-grid-2 masonry-grid fade-in-progressively" data-grid-ratio="1">
