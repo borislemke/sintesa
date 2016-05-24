0c11026837d3666914971aefc3132d9af666d32e
borislemke <dimaslemke@gmail.com>
2016-05-24 22:00:03 +0800

    Promo rules

--- resources/views/modules/custom/accommodation_promo.blade.php
+++ resources/views/modules/custom/accommodation_promo.blade.php
@@ -1,4 +1,38 @@
-<!-- module: lightbox.gallery_column -->
+<!-- module: custom.accommodation_promo -->
+<!--
+DATES
+<?php
+
+// Best: today until tomorrow
+$today = date('Y-n-j');
+$tomorrow = Date('Y-n-j', strtotime("+1 days"));
+$day_after_tomorrow = Date('Y-n-j', strtotime("+2 days"));
+// Advance Purchase: two days starting today in a week
+$one_week_from_today = Date('Y-n-j', strtotime("+7 days"));
+$two_days_after_one_week_from_today = Date('Y-n-j', strtotime("+9 days"));
+// Early bird: two days starting today in 30 days
+$thirty_days_from_today = Date('Y-n-j', strtotime("+31 days"));
+$two_days_after_thirty_days_from_today = Date('Y-n-j', strtotime("+33 days"));
+// Long stay promotion: 5 days duration minimum
+$five_days_from_today = Date('Y-n-j', strtotime("+6 days"));
+
+echo "today: " . $today . "\n";
+echo "tomorrow: " . $tomorrow . "\n";
+echo "one_week_from_today: " . $one_week_from_today . "\n";
+echo "two_days_after_one_week_from_today: " . $two_days_after_one_week_from_today . "\n";
+echo "thirty_days_from_today: " . $thirty_days_from_today . "\n";
+echo "two_days_after_thirty_days_from_today: " . $two_days_after_thirty_days_from_today . "\n";
+echo "five_days_from_today: " . $five_days_from_today . "\n";
+
+$dates = [
+    "https://www.book-secure.com/index.php?s=results&arrival=" . $one_week_from_today . "&departure=" . $two_days_after_one_week_from_today . "&property=idbal28401&rooms=1&locale=en_GB&currency=IDR&stid=3gthgk0hl&adults1=2",
+    "https://www.book-secure.com/index.php?s=results&arrival=" . $today . "&departure=" . $day_after_tomorrow . "&property=idbal28401&rooms=1&locale=en_GB&currency=IDR&stid=3gthgk0hl&adults1=2",
+    "https://www.book-secure.com/index.php?s=results&arrival=" . $thirty_days_from_today . "&departure=" . $two_days_after_thirty_days_from_today . "&property=idbal28401&rooms=1&locale=en_GB&currency=IDR&stid=3gthgk0hl&adults1=2",
+    "https://www.book-secure.com/index.php?s=results&arrival=" . $tomorrow . "&departure=" . $five_days_from_today . "&property=idbal28401&rooms=1&locale=en_GB&currency=IDR&stid=3gthgk0hl&adults1=2"
+];
+?>
+/DATES
+-->
 <div class="section-block portfolio-5 small-margins">
     <div class="row">
         <div class="column width-3 offset-1">
@@ -16,7 +50,7 @@
                     data-hover-bkg-color="#e5e7e9" data-hover-bkg-opacity=".98">
                     <a data-toolbar="Promotion" data-group="project-1" data-caption="{{ $item->title }}"
                         data-image-url="media{{ $item->image }}"
-                        href="{{ $item->promo_link }}" data-lightbox-animation="slideInBottom">
+                        href="{{ $dates[$i] }}" target="_blank" data-lightbox-animation="slideInBottom">
                         <img src="media/{{ $item->image }}" alt=""/>
                     </a>
                 </div>
@@ -27,4 +61,4 @@
     </div>
 </div>
 </div>
-<!-- module_end: lightbox.gallery_column -->
+<!-- module_end: custom.accommodation_promo -->
