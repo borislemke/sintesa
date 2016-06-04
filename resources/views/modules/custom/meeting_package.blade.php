76d23082e9379547382aa9d344cb0c5a39297c17
lewiswebber <hello@fleava.com>
2016-06-04 20:34:39 +0800

    responsive

--- resources/views/modules/custom/meeting_package.blade.php
+++ resources/views/modules/custom/meeting_package.blade.php
@@ -8,82 +8,82 @@
         			<td><strong class="color-gold">Meeting Room</strong></td>
         			<td><strong class="color-gold">Size</strong></td>
         			<td><strong class="color-gold">Cocktail</strong></td>
-        			<td><strong class="color-gold">Theatre</strong></td>
-        			<td><strong class="color-gold">Classroom</strong></td>
-        			<td><strong class="color-gold">Banquet</strong></td>
-        			<td><strong class="color-gold">U-Shape</strong></td>
+        			<td class="hide-on-mobile"><strong class="color-gold">Theatre</strong></td>
+        			<td class="hide-on-mobile"><strong class="color-gold">Classroom</strong></td>
+        			<td class="hide-on-mobile"><strong class="color-gold">Banquet</strong></td>
+        			<td class="hide-on-mobile"><strong class="color-gold">U-Shape</strong></td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Exhibition Hall</span></td>
         			<td>495m2</td>
         			<td>600</td>
-        			<td>500</td>
-        			<td>360</td>
-        			<td>250</td>
-        			<td>-</td>
+        			<td class="hide-on-mobile">500</td>
+        			<td class="hide-on-mobile">360</td>
+        			<td class="hide-on-mobile">250</td>
+        			<td class="hide-on-mobile">-</td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Kunti</span></td>
         			<td>213m2</td>
         			<td>250</td>
-        			<td>243</td>
-        			<td>150</td>
-        			<td>100</td>
-        			<td>69</td>
+        			<td class="hide-on-mobile">243</td>
+        			<td class="hide-on-mobile">150</td>
+        			<td class="hide-on-mobile">100</td>
+        			<td class="hide-on-mobile">69</td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Drupadi</span></td>
         			<td>74m2</td>
         			<td>150</td>
-        			<td>204</td>
-        			<td>90</td>
-        			<td>-</td>
-        			<td>54</td>
+        			<td class="hide-on-mobile">204</td>
+        			<td class="hide-on-mobile">90</td>
+        			<td class="hide-on-mobile">-</td>
+        			<td class="hide-on-mobile">54</td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Srikandi</span></td>
         			<td>55m2</td>
         			<td>-</td>
-        			<td>54</td>
-        			<td>27</td>
-        			<td>-</td>
-        			<td>21</td>
+        			<td class="hide-on-mobile">54</td>
+        			<td class="hide-on-mobile">27</td>
+        			<td class="hide-on-mobile">-</td>
+        			<td class="hide-on-mobile">21</td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Arimbi</span></td>
         			<td>55m2</td>
         			<td>-</td>
-        			<td>54</td>
-        			<td>27</td>
-        			<td>-</td>
-        			<td>21</td>
+        			<td class="hide-on-mobile">54</td>
+        			<td class="hide-on-mobile">27</td>
+        			<td class="hide-on-mobile">-</td>
+        			<td class="hide-on-mobile">21</td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Sumbadra</span></td>
         			<td>45m2</td>
         			<td>56</td>
-        			<td>43</td>
-        			<td>26</td>
-        			<td>-</td>
-        			<td>18</td>
+        			<td class="hide-on-mobile">43</td>
+        			<td class="hide-on-mobile">26</td>
+        			<td class="hide-on-mobile">-</td>
+        			<td class="hide-on-mobile">18</td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Madri</span></td>
         			<td>27m2</td>
         			<td>34</td>
-        			<td>28</td>
-        			<td>18</td>
-        			<td>-</td>
-        			<td>13</td>
+        			<td class="hide-on-mobile">28</td>
+        			<td class="hide-on-mobile">18</td>
+        			<td class="hide-on-mobile">-</td>
+        			<td class="hide-on-mobile">13</td>
         		</tr>
         		<tr>
         			<td><span class="color-gold">Amba</span></td>
         			<td>17m2</td>
         			<td>-</td>
-        			<td>18</td>
-        			<td>12</td>
-        			<td>-</td>
-        			<td>9</td>
+        			<td class="hide-on-mobile">18</td>
+        			<td class="hide-on-mobile">12</td>
+        			<td class="hide-on-mobile">-</td>
+        			<td class="hide-on-mobile">9</td>
         		</tr>
 
         	</table>
